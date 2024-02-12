aa53f2ee4bc5245e247a841d28d206f13710ec48
Sri Panyam <sri.panyam@gmail.com>
2024-02-12 13:02:02 -0800

    Able to launch and run a k8s proxy with local PVs

--- k8s_build/templates/apply.sh
+++ k8s_build/templates/apply.sh
@@ -15,18 +15,30 @@ kubectl -n {{PROXY_NAMESPACE}} create secret generic vault-keys \
 
 # Create local PVs if we are in local PV mode - otherwise if we are in say efs do somethign else!
 LOCAL_PV_ROOT={{LOCAL_PV_ROOT}}
+MINIKUBE_MOUNT_ROOT={{MINIKUBE_MOUNT_ROOT}}
+# minikube mount ${LOCAL_PV_ROOT}:${MINIKUBE_MOUNT_ROOT} & 
+
+mkdir -p ${LOCAL_PV_ROOT}/vault/data
+# minikube mount ${LOCAL_PV_ROOT}/vault/data:/vault/data &
+
 echo "Creating local PV folders for cmd-exec..."
 mkdir -p ${LOCAL_PV_ROOT}/cmd-exec/keys
 mkdir -p ${LOCAL_PV_ROOT}/cmd-exec/logs
+kubectl apply -f cmd-exec-storageclass.yaml -n {{PROXY_NAMESPACE}}
 kubectl apply -f cmd-exec-localpvs.yaml -n {{PROXY_NAMESPACE}}
+kubectl apply -f cmd-exec-pvcs.yaml -n {{PROXY_NAMESPACE}}
+
 echo "Creating local PV folders for outpost..."
 mkdir -p ${LOCAL_PV_ROOT}/outpost/jobs
 mkdir -p ${LOCAL_PV_ROOT}/outpost/logs
 mkdir -p ${LOCAL_PV_ROOT}/outpost/sidecar
+if [ ! -f $LOCAL_PV_ROOT/outpost/jobs/daglib.py ]; then
+  cp daglib.py  $LOCAL_PV_ROOT/outpost/jobs/daglib.py 
+fi
+kubectl apply -f outpost-storageclass.yaml -n {{PROXY_NAMESPACE}}
 kubectl apply -f outpost-localpvs.yaml -n {{PROXY_NAMESPACE}}
+kubectl apply -f outpost-pvcs.yaml -n {{PROXY_NAMESPACE}}
 
 kubectl apply -f vault.yaml -n {{PROXY_NAMESPACE}}
-kubectl apply -f cmd-exec-pvcs.yaml -n {{PROXY_NAMESPACE}}
 kubectl apply -f cmd-exec-main.yaml -n {{PROXY_NAMESPACE}}
-kubectl apply -f outpost-pvcs.yaml -n {{PROXY_NAMESPACE}}
 kubectl apply -f outpost-main.yaml -n {{PROXY_NAMESPACE}}
--- k8s_build/templates/deleteall.sh
+++ k8s_build/templates/deleteall.sh
@@ -1,3 +1,8 @@
+kubectl delete namespace {{PROXY_NAMESPACE}}
+kubectl delete pv {{PROXY_NAMESPACE}}-cmd-exec-keys-volume
+kubectl delete pv {{PROXY_NAMESPACE}}-cmd-exec-logs-volume
+kubectl delete pv {{PROXY_NAMESPACE}}-outpost-jobs-volume
+kubectl delete pv {{PROXY_NAMESPACE}}-outpost-logs-volume
+kubectl delete pv {{PROXY_NAMESPACE}}-outpost-sidecar-volume
 kubectl delete pvc --all -n {{PROXY_NAMESPACE}}
 kubectl delete all --all -n {{PROXY_NAMESPACE}}
-kubectl delete namespace {{PROXY_NAMESPACE}}
