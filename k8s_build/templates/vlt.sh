5ae878210feaeaf02a8f9ef7f0301dbfe8dfaf2c
Sri Panyam <sri.panyam@gmail.com>
2024-02-12 13:19:09 -0800

    switching to dkvault

--- k8s_build/templates/vlt.sh
+++ k8s_build/templates/vlt.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-CONTAINER_ID=$(kubectl get pods -n {{PROXY_NAMESPACE}} | grep frontend | awk '{print $1}')
-CMD="kubectl exec -it ${CONTAINER_ID} -n {{PROXY_NAMESPACE}} -- python src/vlt.py"
+CONTAINER_ID=$(kubectl get pods -n {{PROXY_NAMESPACE}} | grep cmd-exec | awk '{print $1}')
+CMD="kubectl -n {{PROXY_NAMESPACE}} exec -it ${CONTAINER_ID} -- dkvault --vault-url=https://vault:8200 --vault-unseal-tokens-file ./src/keys/vault_unseal.keys $*"
 eval $CMD
