6ddd937bb6067ce7cc148b28a7e8a41fe9e2b9ef
Sri Panyam <sri.panyam@gmail.com>
2024-02-16 14:11:57 -0800

    cleanups

--- k8s_build/templates/storage/efs/policies.sh
+++ k8s_build/templates/storage/efs/policies.sh
@@ -1,4 +1,6 @@
 
+helm uninstall aws-efs-csi-driver -n kube-system
+
 export cluster_name={{EKS_CLUSTER_NAME}}
 export role_name=AmazonEKS_EFS_CSI_DriverRole
 export OIDC=`aws --region {{AWS_REGION}} eks describe-cluster --name $cluster_name --query "cluster.identity.oidc.issuer" --output text | grep http | sed -e 's/http.*\/id\///g'`
@@ -16,7 +18,7 @@ aws iam attach-role-policy \
   --policy-arn arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy \
   --role-name $role_name
 
-echo "Creating service account..."
+echo "Creating service account for node..."
 eksctl create iamserviceaccount \
     --region {{AWS_REGION}}     \
     --name efs-csi-controller-sa \
@@ -29,11 +31,9 @@ eksctl create iamserviceaccount \
 
 TRUST_POLICY=$(aws iam get-role --role-name $role_name --query 'Role.AssumeRolePolicyDocument' | \
     sed -e 's/efs-csi-controller-sa/efs-csi-*/' -e 's/StringEquals/StringLike/')
-
 aws iam update-assume-role-policy --role-name $role_name --policy-document "$TRUST_POLICY"
 
 helm upgrade --install aws-efs-csi-driver --namespace kube-system aws-efs-csi-driver/aws-efs-csi-driver \
              --set controller.serviceAccount.create=false \
              --set controller.serviceAccount.name=efs-csi-controller-sa \
              --set image.repository=602401143452.dkr.ecr.{{AWS_REGION}}.amazonaws.com/eks/aws-efs-csi-driver
-
--- k8s_build/templates/storage/efs/storage.sh
+++ k8s_build/templates/storage/efs/storage.sh
@@ -1,6 +1,13 @@
 # Create local PVs if we are in local PV mode - otherwise if we are in say efs do somethign else!
 #
 #
+if [ x"$1" = "xdelete" ]; then
+  kubectl delete -f storageclasses-efs.yaml
+  kubectl delete -f vault-pvcs-efs.yaml -n {{PROXY_NAMESPACE}}
+  kubectl delete -f cmd-exec-pvcs-efs.yaml -n {{PROXY_NAMESPACE}}
+  kubectl delete -f outpost-pvcs-efs.yaml -n {{PROXY_NAMESPACE}}
+fi
+
 # Create all storage classes
 kubectl apply -f storageclasses-efs.yaml
 
