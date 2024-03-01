95029610bc031279b08d976a77d6659d57cefe4b
Sri Panyam <sri.panyam@gmail.com>
2024-03-01 13:19:20 -0800

    docker via dnf

--- install_rhel.sh
+++ install_rhel.sh
@@ -4,6 +4,11 @@
 sudo yum update -y
 sudo yum install -y make git docker unzip python3-pip ca-certificates gnupg
 sudo yum install -y docker-ce --allowerasing
+
+sudo dnf install --nobest docker-ce
+sudo dnf install https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm
+sudo dnf install docker-ce
+
 sudo usermod -aG docker ${USER}
 sudo systemctl enable --now docker
 systemctl is-active docker
