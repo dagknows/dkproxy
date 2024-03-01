fff2e84c863bb719f1e19995179037325e52413c
Sri Panyam <sri.panyam@gmail.com>
2024-03-01 11:06:34 -0800

    system specific installers

---
+++ install_amazonlinux.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+## Installer for Ubuntu
+sudo apt-get update
+sudo apt-get install -y make
+sudo apt-get install -y make docker.io docker docker-compose unzip python3-pip docker-compose-v2
+echo "Installing Docker Repos..."
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+if [ ! -f /etc/apt/keyrings/docker.gpg ]; then
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg ;
+fi
+
+echo "Adding user to docker user group..."
+sudo usermod -aG docker ${USER}
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+make prepare
+
+pip install dagknows --force-reinstall
