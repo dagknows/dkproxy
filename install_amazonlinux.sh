ec36fffec8d2d89b53b03ca265963ab9b6734727
Sri Panyam <sri.panyam@gmail.com>
2024-03-01 11:53:07 -0800

    Amazon linux instructions

--- install_amazonlinux.sh
+++ install_amazonlinux.sh
@@ -9,6 +9,13 @@ echo "Adding user to docker user group..."
 sudo usermod -aG docker ${USER}
 sudo chkconfig docker on
 
+echo "Installing Docker Compose Plugin
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+mkdir -p $DOCKER_CONFIG/cli-plugins
+curl -SL https://github.com/docker/compose/releases/download/v2.24.6/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+
+
 make prepare
 
 pip install dagknows --force-reinstall
