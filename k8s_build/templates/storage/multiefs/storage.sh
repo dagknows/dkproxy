a75e265f6451b3d8a8d4dd4b13bee28fede9ad5a
Sri Panyam <sri.panyam@gmail.com>
2024-03-13 10:47:27 -0700

    Seperating pvc and pv defs to make deletions easier

--- k8s_build/templates/storage/multiefs/storage.sh
+++ k8s_build/templates/storage/multiefs/storage.sh
@@ -2,20 +2,26 @@
 #
 #
 if [ x"$1" = "xdelete" ]; then
-  kubectl delete -f storageclasses-efs.yaml
   kubectl delete -f vault-pvcs-efs.yaml -n {{PROXY_NAMESPACE}}
   kubectl delete -f cmd-exec-pvcs-efs.yaml -n {{PROXY_NAMESPACE}}
   kubectl delete -f outpost-pvcs-efs.yaml -n {{PROXY_NAMESPACE}}
+  kubectl delete -f vault-pvs-efs.yaml
+  kubectl delete -f cmd-exec-pvs-efs.yaml
+  kubectl delete -f outpost-pvs-efs.yaml
+  kubectl delete -f storageclasses-efs.yaml
 fi
 
 # Create all storage classes
 kubectl apply -f storageclasses-efs.yaml
 
 echo "Dynamic PVCs with a hardcoded EFS ID and AccessPoint for vault..."
+kubectl apply -f vault-pvs-efs.yaml -n {{PROXY_NAMESPACE}}
 kubectl apply -f vault-pvcs-efs.yaml -n {{PROXY_NAMESPACE}}
 
 echo "Dynamic PVCs with a hardcoded EFS ID and AccessPoint for Cmd-Exec..."
+kubectl apply -f cmd-exec-pvs-efs.yaml -n {{PROXY_NAMESPACE}}
 kubectl apply -f cmd-exec-pvcs-efs.yaml -n {{PROXY_NAMESPACE}}
 
 echo "Dynamic PVCs with a hardcoded EFS ID and AccessPoint for outpost..."
+kubectl apply -f outpost-pvs-efs.yaml -n {{PROXY_NAMESPACE}}
 kubectl apply -f outpost-pvcs-efs.yaml -n {{PROXY_NAMESPACE}}
